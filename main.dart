2aaba889c94214f21656691d8741b4d2d966fb6e
Túlio <163613757+tuliocns@users.noreply.github.com>
2025-11-18 02:24:51 +0000

    2025-2-POO-DESC-IRRF

--- main.dart
+++ main.dart
@@ -1,25 +1,43 @@
-import 'models/funcionario.dart';
-import 'models/tabela_irrf.dart';
-import 'services/calculadora_irrf.dart';
+class FaixaIRRF {
+  double minimo;
+  double maximo;
+  double aliquota;
+  double deducao;
+
+  FaixaIRRF(this.minimo, this.maximo, this.aliquota, this.deducao);
+
+  bool contem(double salario) {
+    return salario >= minimo && salario <= maximo;
+  }
+}
+
+class CalculadoraIRRF {
+  List<FaixaIRRF> faixas = [
+    FaixaIRRF(0, 2428.80, 0.0, 0.0),
+    FaixaIRRF(2428.81, 2826.65, 0.075, 182.16),
+    FaixaIRRF(2826.66, 3751.05, 0.15, 394.16),
+    FaixaIRRF(3751.06, 4664.68, 0.225, 675.49),
+    FaixaIRRF(4664.69, double.infinity, 0.275, 908.73),
+  ];
+
+  double calcularIRRF(double salarioBase) {
+    FaixaIRRF faixa = faixas.firstWhere((f) => f.contem(salarioBase));
+    double ir = (salarioBase * faixa.aliquota) - faixa.deducao;
+    return ir < 0 ? 0 : double.parse(ir.toStringAsFixed(2));
+  }
+}
 
 void main() {
-  // Exemplo do professor:
   double salarioBruto = 3000.00;
-  double descontoInss = 258.83;
+  double inss = 258.83;
 
-  Funcionario funcionario = Funcionario(
-    salarioBruto: salarioBruto,
-    inss: descontoInss,
-  );
+  double salarioBase = salarioBruto - inss;
 
-  double salarioBase = funcionario.salarioBase;
+  CalculadoraIRRF calc = CalculadoraIRRF();
+  double irrf = calc.calcularIRRF(salarioBase);
 
-  CalculadoraIRRF calculadora = CalculadoraIRRF(TabelaIRRF());
-
-  double irrf = calculadora.calcular(salarioBase);
-
-  print("Salário Bruto: R\$ ${salarioBruto.toStringAsFixed(2)}");
-  print("INSS: R\$ ${descontoInss.toStringAsFixed(2)}");
-  print("Salário Base: R\$ ${salarioBase.toStringAsFixed(2)}");
-  print("IRRF: R\$ ${irrf.toStringAsFixed(2)}");
+  print("Salário Bruto: $salarioBruto");
+  print("INSS: $inss");
+  print("Salário Base: $salarioBase");
+  print("IRRF: $irrf");
 }
